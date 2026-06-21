c00462842fbfb98d5f9d43192b9e466c3cef9c6b
Alex <alex@Alexs-MacBook-Pro-2.local>
2026-06-21 11:53:19 -0700

    fix: remove bad ts-expect-error comment

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-// @ts-expect-error – vision types may lag behind sanity version
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./sanity/schemaTypes";
 
